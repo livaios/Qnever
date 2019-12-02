9dfe6531b89853e3bacbfa09eb55bb0ac528696e
hbadr97 <hussein.a.badr1997@gmail.com>
2019-12-02 20:40:28 +0100

    adds rest

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import(
+	"github.com/gorilla/mux"
+	"net/http"
+	"encoding/json"
+)
+
+type User struct {
+	ID int `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Entity struct {
+	ID int `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Queue struct {
+	ID int `json:"id"`
+	Owner_id int `json:"Entity.id"`
+	head int `json:head`
+}
+
+func getAllEntities(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range users {
+	  if item.ID == params["id"] {
+		json.NewEncoder(w).Encode(item)
+		break
+	  }
+	  return
+	}
+	json.NewEncoder(w).Encode(users)
+  }
+
+func main(){
+	router := mux.NewRouter()
+
+	router.HandleFunc("/getAllEntities", getAllEntities).Methods("GET")
+
+	http.ListenAndServe(":9090", router)
+}
